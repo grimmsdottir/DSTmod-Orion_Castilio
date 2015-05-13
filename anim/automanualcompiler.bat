e548e611ca794358e3627fffb0a293d21112467e
grimmsdottir <grimmsdottir93@gmail.com>
2015-05-13 11:53:20 +0800

    updated the automanualcompiler
    changed it to reflect the new github directory

--- anim/automanualcompiler.bat
+++ anim/automanualcompiler.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools Release\mod_tools\scml.exe" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\orion\exported\orion\orion.scml" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\orion"
-"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools Release\mod_tools\scml.exe" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\orion\exported\ghost_orion_build\ghost_orion_build.scml" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\orion"
+"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools Release\mod_tools\scml.exe" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\DSTmod-Orion_Castilio\exported\orion\orion.scml" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\DSTmod-Orion_Castilio"
+"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools Release\mod_tools\scml.exe" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\DSTmod-Orion_Castilio\exported\ghost_orion_build\ghost_orion_build.scml" "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together Beta\mods\DSTmod-Orion_Castilio"
 
 "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools Release\mod_tools\autocompiler.exe"
 
